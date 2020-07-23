10950dbe0a0f00dfabe1ffb4fd2b30a2852fc9ce
sobolezm <sobolezm@gmail.com>
2020-07-23 15:03:18 +0600

    Needed help with routing layouts components

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app-main-layout">
+    <nav class="nav-bar blue lighten-1"></nav>
+    <ul class="sidenav app-sidenav open"></ul>
+
+    <main class="app-content">
+      <div class="app-page">
+        <router-view/>
+      </div>
+    </main>
+  </div>
+</template>
---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <form class="card auth-card">
+    <div class="card-content">
+      <span class="card-title">Вход</span>
+      <div class="input-field">
+        <input type="text" id="username" class="validate">
+        <label for="username">Введите имя пользователя:</label>
+        <small class="helper-text invalid">failure</small>
+      </div>
+      <div class="input-field">
+        <input type="password" id="password" class="validate">
+        <label for="password">Введите пароль:</label>
+        <small class="helper-text invalid">failure</small>
+      </div>
+      <div class="card-action">
+        <div>
+          <button
+                  class="btn waves-effect waves-light auth-submit"
+                  type="submit"
+          >
+            Войти
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
+        <p class="center">
+          Если у вас нет аккаунта?
+          <router-link href="/">Зарегистрировать</router-link>
+        </p>
+      </div>
+    </div>
+  </form>
+</template>
